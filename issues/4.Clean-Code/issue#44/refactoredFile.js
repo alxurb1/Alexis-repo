5efddf2dca08648f0706cbb5a7208b634b98d6a2
Oscar Martínez <lxsurbaez@gmail.com>
2025-09-05 23:04:02 -0600

    docs(git): adding a reflection on the importance of using clear variables name (Issue #44)

---
+++ issues/4.Clean-Code/issue#44/refactoredFile.js
@@ -0,0 +1,9 @@
+function calculateTriangleArea(base, height) {
+  let area = base * height;
+  let triangleArea = area / 2;
+  console.log("The triangle area is: " + triangleArea);
+}
+
+let baseValue = 30;
+let heightValue = 40;
+calculateTriangleArea(baseValue, heightValue);
---
+++ issues/4.Clean-Code/issue#44/unclearVariableNames.js
@@ -0,0 +1,9 @@
+function calc(a, b) {
+  let c = a * b;
+  let d = c / 2;
+  console.log("Result: " + d);
+}
+
+let a = 10;
+let b = 20;
+calc(a, b);
