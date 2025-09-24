1f589a1a771f7e9d1c0b4f9290144ce6deedbf61
Oscar Martínez <166931452+alxurb1@users.noreply.github.com>
2025-09-23 23:37:58 -0600

    docs(git): implementing language handling with i18next and adding some .md file as a reflection

--- issues/5.React-Fundamentals/react-project/src/main.jsx
+++ issues/5.React-Fundamentals/react-project/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDom from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import "./config/i18n.js";
 
 ReactDom.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
