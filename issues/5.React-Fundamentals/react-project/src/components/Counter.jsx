f77ad1bfce9e2f69879c5af4406ae84d67609cb0
Oscar Martínez <166931452+alxurb1@users.noreply.github.com>
2025-09-28 01:22:12 -0600

    docs(git): adding an implementation of redux devtools and redux debugging and a .md file

--- issues/5.React-Fundamentals/react-project/src/components/Counter.jsx
+++ issues/5.React-Fundamentals/react-project/src/components/Counter.jsx
@@ -1,28 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, reset } from "./CounterSlice";
 
 export default function Counter() {
-	const [counter, setCount] = useState(0);
+	const counter = useSelector(state => state.counter.value);
+	const dispatch = useDispatch();
+
 	return (
 		<div className='flex flex-col items-center'>
-			<h1 className='text-3xl font-bold mb-9'>Counter Using useState Hooks</h1>
+			<h1 className='text-3xl font-bold mb-9'>Counter Using Redux Toolkit</h1>
 			<h1 className='text-3xl font-bold text-blue-600 my-4'>{counter}</h1>
 			<div className='flex gap-4'>
 				<button
-					onClick={() => setCount(counter + 1)}
+					onClick={() => dispatch(increment())}
 					className='px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition active:bg-green-700 active:scale-95 '
 				>
 					Increment
 				</button>
 
 				<button
-					onClick={() => setCount(counter - 1)}
+					onClick={() => dispatch(decrement())}
 					className='px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600 transition active:bg-gray-700 active:scale-95 '
 				>
 					Decrement
 				</button>
 
 				<button
-					onClick={() => setCount(0)}
+					onClick={() => dispatch(reset())}
 					className='px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition active:bg-red-700 active:scale-95 '
 				>
 					Reset
--- issues/5.React-Fundamentals/react-project/src/main.jsx
+++ issues/5.React-Fundamentals/react-project/src/main.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import ReactDom from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
 import "./index.css";
 import App from "./App.jsx";
 import "./config/i18n.js";
 
-ReactDom.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
 	</React.StrictMode>
 );
