89000351e307d1992670d5650aefe0c93a4743a9
Oscar Martínez <166931452+alxurb1@users.noreply.github.com>
2025-09-28 22:15:04 -0600

    docs(git): implementing unit testing using React Testing Library

--- issues/5.React-Fundamentals/react-project/src/pages/Profile.jsx
+++ issues/5.React-Fundamentals/react-project/src/pages/Profile.jsx
@@ -1,8 +1,10 @@
+import Message from "../components/Message";
+
 export default function Profile() {
 	return (
 		<div className='flex flex-col items-center'>
 			<div className='my-4 text-2xl font-bold'>
-				<h1>Profile Page</h1>
+				<Message />
 			</div>
 		</div>
 	);
